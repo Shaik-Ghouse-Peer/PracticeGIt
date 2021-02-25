89a9d70f359632cc426c59bcb44c5ac47ee7c146
Shaik Ghouse Peer <shaik.g.peer@intimetec.com>
2021-02-25 19:52:42 +0530

    deleted core added framework project

---
+++ NunitSample/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NunitSample
+{
+    [TestFixture]
+    public class Class1
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("SetUP");
+        }
+        [Test]
+        public void TestCaseOne()
+        {
+            Console.WriteLine("Test Case One");
+        }
+        [OneTimeTearDown]
+        public void ClearResources()
+        {
+            Console.WriteLine("Clear Resources");
+        }
+    }
+}
