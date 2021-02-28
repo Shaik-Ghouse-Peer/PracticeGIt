4004d5176b985719f87309a13fdfb22cb364e465
Shaik Ghouse Peer <shaik.g.peer@intimetec.com>
2021-02-28 15:05:54 +0530

    Added c_sharp Calculator NUnitTest Project

---
+++ CalculatorTesting/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTesting
+{
+    class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public int Sub(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
---
+++ CalculatorTesting/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculatorTesting
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _calculator;
+
+        [OneTimeSetUp]
+        public void SetUpEnvironment()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            int expectedSum = 30;
+            Assert.AreEqual(expectedSum, _calculator.Add(10, 20));
+        }
+        [Test]
+        public void SubTest()
+        {
+            int expectedSubstraction = 20;
+            Assert.AreEqual(expectedSubstraction, _calculator.Sub(40, 20));
+        }
+        [Test]
+        public void MultiplyTest()
+        {
+            int expectedMultiplication = 100;
+            Assert.AreEqual(expectedMultiplication, _calculator.Multiply(10, 10));
+        }
+    }
+}
